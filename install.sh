2eff918c337816b4cf9bfae20245d3c87420d52c
Vi B-P <mookbot@gmail.com>
2020-01-30 10:55:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
-sudo apt update
-sudo apt install git
-
-
-mkdir .ssh
-
-scp /home/violet/.ssh/githubkey /home/violet/.ssh/config /home/violet/.ssh/authorized_keys pi@raspberrypi.escher.lan
-
-
-
-
-
-
-python3 -m venv .env
-source
-pip3 install RPI.GPIO adafruit-blinka
+echo "Cloning repo"
+apt-get update
+apt install git
+# git clone https://github.com/violetbp/zappy.git
+cd zappy
+echo "installing"
+bash serviceInstall.sh
